1c89ccf9b5d1aabe776c3ffef1831d00716354af
munstein <eduardo.munstein@live.com>
2018-02-23 23:47:32 -0300

    Removing some code

--- app/src/main/java/com/munstein/passwordreset/base/BaseActivity.kt
+++ app/src/main/java/com/munstein/passwordreset/base/BaseActivity.kt
@@ -1,14 +1,8 @@
 package com.munstein.passwordreset.base
 
-import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 
 /**
  * Created by @Munstein on 23/02/2018.
  */
-open class BaseActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-    }
-}
+open class BaseActivity : AppCompatActivity()
