7448e26b4268944d05baf6021af69bd7254b962f
munstein <eduardo.munstein@live.com>
2018-02-23 22:14:07 -0300

    Changing some colors

--- app/src/main/java/com/munstein/passwordreset/base/BaseActivity.kt
+++ app/src/main/java/com/munstein/passwordreset/base/BaseActivity.kt
@@ -1,9 +1,15 @@
 package com.munstein.passwordreset.base
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 
 /**
  * Created by @Munstein on 23/02/2018.
  */
 open class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
 }
